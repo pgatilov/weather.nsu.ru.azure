e0bdc0c0b0457f4dab4ee79341782bfc08123fd6
Pavel Gatilov <pgatilov@outlook.com>
2014-04-22 01:13:22 +0700

    I-2: working on temperature graph loading

---
+++ weather.nsu.ru.azure/Controllers/Api/TemperatureApiController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace weather.nsu.ru.azure.Controllers.Api
+{
+    /// <summary>
+    /// Provides access to temperature data.
+    /// </summary>
+    public class TemperatureApiController : ApiController
+    {
+        public 
+    }
+}
---
+++ weather.nsu.ru.azure/Models/TemperatureRecord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using weather.nsu.ru.azure.Data;
+
+namespace weather.nsu.ru.azure.Models
+{
+    public class TemperatureChartPoint
+    {
+        public DateTime Taken { get; set; }
+
+        public float Temperature { get; set; }
+
+        public Unit Unit { get; set; }
+    }
+}
